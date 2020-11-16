58d5c89523062815d2742bd8d467ae3d51c38540
Matt Bimpson <matt@Matts-MacBook-Pro.local>
2020-11-16 10:38:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Chitchat - a realtime chat app made with Socket.IO
+
+# How to:
+Start the server with "node main.js"
+Visit the site in your browser (http://localhost:7575)
+Visit the site again with a separate browser window to see the conversation working.
---
+++ main.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+const port = 80;
+var clients = [];
+
+app.get('/', function(_req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+function errorHandler (err, _req, res, _next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+app.use(errorHandler);
+
+app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io/client-dist/'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
+io.on('connection', function(socket) {
+    clients.push(socket.id);
+
+    socket.on('chat message', function(msg){
+        if (msg === '') {
+          return
+        };
+        var name = socket.nickname || 'unknown';
+        io.emit('chat message', name + ': ' + msg);
+        LogMessage(name, msg);
+    })
+
+    socket.on('send-nickname', function(nickname) {
+        if (nickname === '') {
+          return
+        };
+        socket.nickname = nickname;
+        clients.push({nickname: socket.nickname, id: socket.id});
+        UpdateClientNickname(socket.id, socket.nickname);
+
+        var msg = 'User ' + socket.nickname + ' is in the house...';
+        io.emit('notification', msg);
+    })
+})
+
+http.listen(port, function() {
+    console.log('listenening on port ' + port);
+})
+
+function UpdateClientNickname(id, name) {
+    clients.forEach(function(c) {
+        if (c.id === id) {
+            c.nickname = name;
+        }
+    })
+}
+
+function LogMessage(nickname, message) {
+    fs.appendFile('chat.txt', GetDateTime() + ',' + nickname + ',' + message + '\r\n', function(_err) {});
+}
+
+function GetDateTimeShort(datetime) {
+  var dateStr = datetime.getFullYear() + '-' + parseInt(datetime.getMonth() + 1) + '-' + datetime.getDate();
+  var timeStr = datetime.getHours() + ':' + datetime.getMinutes() + ':' + datetime.getSeconds();
+  return dateStr + ' ' + timeStr;
+}
+
+function GetDateTime() {
+  var now = new Date();
+  return GetDateTimeShort(now);
+}
